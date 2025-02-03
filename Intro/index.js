a85202098e85b41785a5e14a2ec611f3213dd2ef
Kipngetich222 <123062517+Kipngetich222@users.noreply.github.com>
2025-02-03 05:59:45 +0300

    react trial

---
+++ Intro/index.js
@@ -0,0 +1,9 @@
+function Greeting({ name })(
+    return <h1>Hello, {name}</h1>;
+
+    export defout function Appexport default functio Appexport(){
+        return <Greeting name = "worl">
+    }
+
+
+)
