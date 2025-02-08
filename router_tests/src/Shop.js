3c9d27284955dba36e7411a578712552828a3e17
Kipngetich222 <123062517+Kipngetich222@users.noreply.github.com>
2025-02-08 05:38:38 +0300

    dynamic routes

--- router_tests/src/Shop.js
+++ router_tests/src/Shop.js
@@ -5,15 +5,24 @@ function Shop() {
     useEffect(()=>{
         fetchItems();
     },[]);
+
+    const [items, setItems]  = useState([]);
     const fetchItems = async ()=> {
         const data = await fetch("https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get");
-        console.log(data);
         
-    }
+        const items = await data.json();
+        console.log(items.items);
+        setItems(items.items);
+        
+        
+    };
 
   return (
     <div className="App">
       <h1>Shop page</h1>
+      {items.map(item =>{
+        <h1 key={item.itemid}>{item.name}</h1>
+      })}
     </div>
   );
 }
